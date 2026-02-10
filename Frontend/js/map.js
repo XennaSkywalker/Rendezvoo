d1a2c690ee0fcaf494f59d39ff721bf48b81e9b3
OnyX2811 <87792634+OnyX2811@users.noreply.github.com>
2026-02-10 10:15:07 +0530

    v1.1: Added meetup form on map click

--- Frontend/js/map.js
+++ Frontend/js/map.js
@@ -3,24 +3,33 @@ async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement, PinElement } =
     await google.maps.importLibrary("marker");
+
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
-    center: { lat: -25.363882, lng: 131.044922 },
+    center: { lat: 24, lng: 90 },
     mapId: "DEMO_MAP_ID",
   });
 
+  const meetupFormContainer = document.getElementById("meetup-form-container");
+  const closeButton = document.getElementById("close-button");
+
   map.addListener("click", (e) => {
-    placeMarkerAndPanTo(e.latLng, map);
+    placeMarker(e.latLng, map);
     displayLatLng(e.latLng);
+    meetupFormContainer.classList.remove("hidden");
+  });
+
+  closeButton.addEventListener("click", () => {
+    meetupFormContainer.classList.add("hidden");
   });
 }
 
-function placeMarkerAndPanTo(latLng, map) {
+//helper functions
+function placeMarker(latLng, map) {
   new google.maps.marker.AdvancedMarkerElement({
     position: latLng,
     map: map,
   });
-  map.panTo(latLng);
 }
 
 function displayLatLng(latLng) {
