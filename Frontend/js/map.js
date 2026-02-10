2cf943e793f7265c754f9581cf10b996c5b8af44
OnyX2811 <87792634+OnyX2811@users.noreply.github.com>
2026-02-10 14:38:38 +0530

    v1.1: Meetup data processing in map.js working, cleaned map.js code a bit

--- Frontend/js/map.js
+++ Frontend/js/map.js
@@ -1,5 +1,11 @@
+//this file is only for handling the UI of the website
+const meetupFormContainer = document.getElementById("meetup-form-container");
+const meetupForm = document.getElementById("meetup-form");
+const closeButton = document.getElementById("close-button");
+const submitButton = document.getElementById("submit-button");
+let latLng;
+
 async function initMap() {
-  // Request needed libraries.
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement, PinElement } =
     await google.maps.importLibrary("marker");
@@ -10,17 +16,32 @@ async function initMap() {
     mapId: "DEMO_MAP_ID",
   });
 
-  const meetupFormContainer = document.getElementById("meetup-form-container");
-  const closeButton = document.getElementById("close-button");
-
   map.addListener("click", (e) => {
+    latLng = e.latLng;
     placeMarker(e.latLng, map);
     displayLatLng(e.latLng);
     meetupFormContainer.classList.remove("hidden");
   });
 
+  initForm();
+}
+
+function initForm() {
   closeButton.addEventListener("click", () => {
     meetupFormContainer.classList.add("hidden");
+  });
+
+  submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const meetupFormData = new FormData(meetupForm);
+    const meetupData = {
+      lat: latLng.lat(),
+      lng: latLng.lng(),
+      meetupName: meetupFormData.get("meetup-name"),
+      meetupDate: meetupFormData.get("meetup-date"),
+      meetupTime: meetupFormData.get("meetup-time"),
+    };
+    console.log(meetupData);
   });
 }
 
@@ -35,5 +56,7 @@ function placeMarker(latLng, map) {
 function displayLatLng(latLng) {
   console.log(JSON.stringify(latLng));
 }
+
+function postMeetupRequest() {}
 
 initMap();
